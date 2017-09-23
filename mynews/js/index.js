78eaf46eb3679b82509251a900c0f0d993716d5e
ganmyds <1535628743@qq,com>
2017-09-24 00:56:15 +0800

    提交我的新闻网站

---
+++ mynews/js/index.js
@@ -0,0 +1,31 @@
+$(function() {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 600) {
+            $('#goto-top').show();
+        } else {
+            $('#goto-top').hide();
+        }
+    })
+
+    $(".content .list-group .item").click(function() {
+        if (!$(this).hasClass("active-red")) {
+            var list = $(".content .list-group .item");
+            var group;
+            for (var i = 0; i < list.length; i++) {
+                group = $(list[i])
+                if (group.hasClass('active-red')) {
+                    group.removeClass("active-red")
+                }
+            }
+            $(this).addClass("active-red")
+        }
+    })
+})
+
+function pageScroll() {
+    window.scrollBy(0, -60);
+    scrolldelay = setTimeout('pageScroll()', 22);
+    if (document.body.scrollTop == 0) {
+        clearTimeout(scrolldelay);
+    }
+};
--- myshopstore/js/index.js
+++ myshopstore/js/index.js
@@ -6,3 +6,19 @@
    })
 
 })
+
+function pageScroll() {
+    window.scrollBy(0, -60);
+    scrolldelay = setTimeout('pageScroll()', 22);
+    if (document.body.scrollTop == 0) {
+        clearTimeout(scrolldelay);
+    }
+};
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 1000){
+        $('#goto-top').show();
+    }else{
+        $('#goto-top').hide();
+    }
+});
